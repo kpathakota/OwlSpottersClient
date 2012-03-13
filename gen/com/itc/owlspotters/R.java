d50712cb23e5ca2f577246291421a72b744353d0
Kiran Pathakota <kpathakota@gmail.com>
2012-03-13 00:07:06 -0500

    after class commit

--- gen/com/itc/owlspotters/R.java
+++ gen/com/itc/owlspotters/R.java
@@ -13,11 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int EditText01=0x7f050006;
+        public static final int EditText02=0x7f050003;
+        public static final int LinearLayout02=0x7f050001;
+        public static final int LinearLayout03=0x7f050004;
+        public static final int LoginButton=0x7f050008;
+        public static final int MainLoginPage=0x7f050000;
+        public static final int PasswordBox=0x7f050005;
+        public static final int ServerBox=0x7f050007;
+        public static final int UsernameBox=0x7f050002;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int Server=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int login=0x7f040005;
+        public static final int password=0x7f040002;
+        public static final int username=0x7f040004;
     }
 }
